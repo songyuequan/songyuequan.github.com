a40f98ab50b50516305a5134a3f886d93d15a584
songyuequan <18210166949@163.com>
2015-03-18 15:40:46 +0800

    jiayuantong

---
+++ jiayuantong/files/home/data.js
@@ -0,0 +1,89 @@
+﻿$axure.loadCurrentPage({
+  "url":"home.html",
+  "generationDate":new Date(1426661052431.53),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable",
+"value"],
+  "page":{
+    "packageId":"f87382e47ffc4c0a9435a4634c6c5b57",
+    "type":"Axure:Page",
+    "name":"Home",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"d5a8e89f5f8a44529fa2fc831dd61894",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":310,
+              "y":12},
+            "size":{
+              "width":411,
+              "height":813}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"e0690217dd014263a3893429eea4ab6c",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":310,
+                  "y":12},
+                "size":{
+                  "width":411,
+                  "height":813}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/home/u0.png"}},
+{
+          "id":"ea6392b2ea3b41fb97dc740b09e0fe81",
+          "label":"",
+          "type":"inlineFrame",
+          "styleType":"inlineFrame",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":359,
+              "y":135},
+            "size":{
+              "width":320,
+              "height":569}},
+          "adaptiveStyles":{
+}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "d5a8e89f5f8a44529fa2fc831dd61894":{
+      "scriptId":"u0"},
+    "e0690217dd014263a3893429eea4ab6c":{
+      "scriptId":"u1"},
+    "ea6392b2ea3b41fb97dc740b09e0fe81":{
+      "scriptId":"u2"}}});
